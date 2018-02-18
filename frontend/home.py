35edcbf59461229bc41497aabeaf6540d21b39cf
Louis <louisgurung@yahoo.com>
2018-02-17 19:05:32 -0600

    added meroproject.html

--- frontend/home.py
+++ frontend/home.py
@@ -10,3 +10,10 @@ def index():
 @frontend_app.route('/sample_page')
 def sample_page():
     return render_template('sample_page.html')
+
+
+@frontend_app.route('/world_123')
+def world():
+    return render_template('meroProject.html')
+
+
